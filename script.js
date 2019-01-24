3c79e6906ce614eda92d3089137ff432889b5735
Trevan Young <trevan.young@gmail.com>
2019-01-24 14:03:58 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//find a way to output the string "I am a rockstar ninja unicorn JS dev" from this array.
+
+
+function arrayToString() {
+    let hyperbole = ["dev ", "JS ", "unicorn ", "ninja ", "rockstar ", "a ", "am ", "I "]
+    hyperbole.toString();
+    newHyperBole= hyperbole.reverse()
+    document.getElementById("array").innerHTML = newHyperBole;
+  }
